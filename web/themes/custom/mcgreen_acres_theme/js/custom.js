9efeaecbfa5db4cebe88b2445915f75f495581d6
Allan Chappell <generalredneck@gmail.com>
2025-09-09 12:43:27 -0500

    finishing first phase of products and catalog

--- web/themes/custom/mcgreen_acres_theme/js/custom.js
+++ web/themes/custom/mcgreen_acres_theme/js/custom.js
@@ -1055,3 +1055,51 @@ $(window).on('load', function(){
 
 });
 })(jQuery);
+
+(function (Drupal, drupalSettings) {
+  // Initiate all Toasts on page.
+  Drupal.behaviors.toast = {
+    attach(context, settings) {
+      once('initToast', '.toast', context).forEach((el) => {
+        const toastList = new bootstrap.Toast(el);
+        toastList.show();
+      });
+    },
+  };
+
+  // Accordion buttons containing Edit links.
+  Drupal.behaviors.accordionButtonLinks = {
+    attach(context, settings) {
+      once(
+        'accordionButttonLinks',
+        '.fieldset-legend.accordion-button a',
+        context,
+      ).forEach((el) => {
+        // Prevent accordion collapse when clicking on links
+        el.addEventListener('click', function (e) {
+          if (e.target.href) {
+            const targetUrl = e.target.href;
+            window.location.href = targetUrl;
+          }
+        });
+      });
+    },
+  };
+
+  // Collapse and accordion if a field is required.
+  Drupal.behaviors.focusRequired = {
+    attach(context, settings) {
+      const inputs = document.querySelectorAll('form .accordion input');
+      [].forEach.call(inputs, function (input) {
+        input.addEventListener('invalid', function (e) {
+          const accordion = input.closest('.collapse');
+          const collapseAccordion = bootstrap.Collapse.getInstance(accordion);
+          if (collapseAccordion) {
+            collapseAccordion.show();
+          }
+        });
+      });
+    },
+  };
+
+})(Drupal, drupalSettings);
